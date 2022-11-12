a91c622aca0b52715b3c969d115b7516b6a0f413
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-11-12 09:55:13 -0800

    Add function Add "handleChange", and "handleSubmit".

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,17 +1,32 @@
 import { useState } from "react";
 
-export const TodoForm = () => {
+export const TodoForm = (props) => {
   const [input, setInput] = useState("");
+
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // props.onSubmit({
+    //   id: Math.floor(Math.random() * 10000),
+    //   text: input
+    // });
+    setInput("");
+  };
 
   return (
     <div>
-      <form className="todo-form">
+      <form className="todo-form" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Add a todo"
           value={input}
           name="text"
           className="todo-input"
+          onChange={handleChange}
         />
         <button className="todo-button">Add todo</button>
       </form>
