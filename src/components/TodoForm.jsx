bb64cfa79dff0bce48717c84ca4b1157abc9504b
Miku Kawata <100392926+mikuweb@users.noreply.github.com>
2022-11-12 10:20:11 -0800

    Add function "addTodo"

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -10,10 +10,11 @@ export const TodoForm = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // props.onSubmit({
-    //   id: Math.floor(Math.random() * 10000),
-    //   text: input
-    // });
+    props.onSubmit({
+      id: Math.floor(Math.random() * 10000),
+      text: input
+    });
+
     setInput("");
   };
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,10 +4,23 @@ import { TodoForm } from "./TodoForm";
 export const TodoList = () => {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+    console.log(todo, ...todos);
+  };
+
   return (
     <div>
       <h1>What's the Plan for Today?</h1>
-      <TodoForm />
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 };
+
+// ----- Question -----
+// if(!todo.text || /^\s*$/.test(todo.text))
